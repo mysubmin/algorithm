acf47c85934db2a3b2f786f27b8dae7d7cd5c65a
mysubmin <134607089+mysubmin@users.noreply.github.com>
2023-11-17 18:58:53 +0900

    [level 1] Title: 직사각형 별찍기, Time: 7.05 ms, Memory: 14.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
+for i in 1 ... b {
+    var str = ""
+    for j in 1 ... a {
+        str += "*"
+    }
+    print(str)
+}
+
